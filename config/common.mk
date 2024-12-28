6f347ba9e5dd4b8acaa409531f120efccffa8383
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2024-12-28 23:55:31 +0300

    Create common.mk

---
+++ config/common.mk
@@ -0,0 +1,48 @@
+# 
+# Copyright (C) 2024 The nor.ai Project
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# 
+# Shit about nor.ai parts
+#
+
+# Ensure that GMS works, creating decent environment both for GMS and microG users.
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    # For Pixel devices and fully de-Googled device trees.
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    # For 99% of device trees (and users).
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+# Securize ADB.
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+    # Who tf daily drives engineering (eng) builds?
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+else
+    # Securize for standart (user) and debugging (userdebug) user builds.
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+endif
+
+# Optimise Java.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+SYSTEM_OPTIMIZE_JAVA ?= true
+SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# Disable vendor restrictions.
+PRODUCT_RESTRICT_VENDOR_FILES := false
